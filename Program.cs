d6fe509da1d97d40988373decc182fe2f0d0a102
inanakalin <111204187+inanakalin@users.noreply.github.com>
2022-10-25 15:56:56 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GenericColections_Lists
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            ArrayList Koleksiyon = new ArrayList();
+
+            Koleksiyon.Add ("İnan");
+            Koleksiyon.Add ("Nebahat");
+            Koleksiyon.Add ("Reşit");
+            Koleksiyon.Add ("Emre");
+            Koleksiyon.Add ("Elif");
+
+            Koleksiyon.Remove ("Emre");
+            int eleman = Koleksiyon.Count;
+
+            System.Console.WriteLine(eleman);
+            foreach (var isim in Koleksiyon)
+            {
+                System.Console.WriteLine(isim);
+            }
+
+          
+
+
+
+        }
+    }
+}
